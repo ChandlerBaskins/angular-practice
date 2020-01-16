6fc5a49f020d90f915a02401523b6af64bef3f2b
chandler.baskins <chandler.baskins@HQ.NSIDE.IO>
2020-01-16 11:22:47 -0600

    initial cards commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I Saw a tree'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'A pic of a mountain'
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking1222',
+      content: 'I Saw a bike'
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking1222',
+      content: 'I Saw a bike'
+    },
+  ]
 }
